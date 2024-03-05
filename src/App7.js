bf91883f9224de27a3be753daee72b597ad307de
KummariSaiteja <saiteja@lyrostech.com>
2024-03-05 19:02:57 +0530

    added onclickapp7

---
+++ src/App7.js
@@ -0,0 +1,22 @@
+// import React from 'react'
+// export default function App7(){
+//     const handleClick=()=>{
+//         alert("Hello World")
+//     }
+//     return(
+//         <div>
+//             <button onClick={handleClick}>Click</button>
+//         </div>
+//     )
+// }
+import React from 'react'
+export default function App7(){
+    const handleClick=(msg)=>{
+        alert(msg)
+    }
+    return(
+        <div>
+            <button onClick={()=>handleClick('Hi John')}>Click</button>
+        </div>
+    )
+}
