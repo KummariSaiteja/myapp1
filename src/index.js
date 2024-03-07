aae98512b32d1f388672d278944de57d8e30341c
KummariSaiteja <saiteja@lyrostech.com>
2024-03-07 14:20:14 +0530

    changed app6

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App7 from './App7';
-import App6 from './App6';
+// import App6 from './App6';
+import App5 from './App5';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App1 {name:"John",age:{30}}/> */}
-    <App6 />
+    <App5 />
   </React.StrictMode>
 );
 
