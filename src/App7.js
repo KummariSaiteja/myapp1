8160d1a01109e893f4ae759d9d43c5cd5df3cbd9
KummariSaiteja <saiteja@lyrostech.com>
2024-03-08 10:35:54 +0530

    react hooks and edit,delete,add object in array

--- src/App7.js
+++ src/App7.js
@@ -11,13 +11,13 @@
 // }
 import React from 'react'
 export default function App7(){
-    const handleClick=(msg)=>{
-        alert(msg)
-    }
-    return(
+     const handleClick=(msg)=>{
+         alert(msg)
+     }
+     return(
         <div>
             <button onClick={()=>handleClick('Hi John')}>Click</button>
         </div>
-    )
-}
+     )
+ }
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App7 from './App7';
 // import App6 from './App6';
-import App5 from './App5';
+// import App5 from './App5';
+// import App12 from './App12';
+import App9 from './App9';
+import App13 from './App13';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App1 {name:"John",age:{30}}/> */}
-    <App5 />
+    {/* <App6 /> */}
+    {/* <App12 /> */}
+    {/* <App9/> */}
+    <App13/>
   </React.StrictMode>
 );
 
