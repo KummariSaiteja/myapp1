b81c9543c933b9cc0f43bd176277a68c5b1d5276
KummariSaiteja <saiteja@lyrostech.com>
2024-03-09 15:52:36 +0530

    react dom

--- src/App15.js
+++ src/App15.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Feeds from "./Feeds";
 import Post from "./Post";
+import Albums from "./Albums";
 import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
 
 export default function App15() {
   return (
     <div>
       <Router>
-        <Link to="/">Feeds</Link> | <Link to="/post">Post</Link>
+        <Link to="/">Feeds</Link> | <Link to="/post">Post</Link> | <Link to="Albums"></Link>
         <Routes>
             <Route path="/" index element={<Feeds/>}></Route>
             <Route path="/post" element={<Post/>}></Route>
