cf6c8197cecf0ddefd0c7da101443f9fac03fdc5
KummariSaiteja <saiteja@lyrostech.com>
2024-03-09 17:03:48 +0530

    class component

---
+++ src/App17.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class App17 extends Component {
+  render() {
+    return (
+      <div>
+        App17
+      </div>
+    )
+  }
+}//class component
+
+ import React from 'react'
+ 
+ export default function App17() {
+   return (
+     <div>
+       
+     </div>
+   )
+ }
+ 
