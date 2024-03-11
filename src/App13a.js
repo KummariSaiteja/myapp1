ca8ff708665f406454040cb7bc56a202f758f652
KummariSaiteja <saiteja@lyrostech.com>
2024-03-11 17:04:50 +0530

    modified

--- src/App13a.js
+++ src/App13a.js
@@ -1,13 +1,31 @@
+// import { useState, createContext, useContext } from "react";
+// import Child1 from "./Child1";
+// export const UserContext = createContext();
+// export default function App13a() {
+//   const [user, setUser] = useState("John");
+//   return (
+//     <>
+//       <UserContext.Provider value={{ user, setUser }}>
+//         <h2>Hello {user} from App13 component</h2>
+//         <Child1 />
+//       </UserContext.Provider>
+//     </>
+//   );
+// }
+
+
 import { useState, createContext, useContext } from "react";
-import Child1 from "./Child1";
+import Home1 from './Home1';
+import Login1 from '.Login1';
+import Register from '.Register';
 export const UserContext = createContext();
 export default function App13a() {
   const [user, setUser] = useState("John");
+  const [flag,setFlag]=useState(0);
   return (
     <>
       <UserContext.Provider value={{ user, setUser }}>
-        <h2>Hello {user} from App13 component</h2>
-        <Child1 />
+        {!flag?<Login1/>:flag==1?<Home1/>: flag==2?<Register/>:<Login1/>}
       </UserContext.Provider>
     </>
   );
--- src/Child1.js
+++ src/Child1.js
@@ -1,14 +1,26 @@
+// import {UserContext} from "./App13a"
+// import { useContext } from "react";
+// export default function Childa() {
+//   const {user,setUser} = useContext(UserContext);
+//   const changeUserName=()=>{
+//     setUser("Cathy")
+//   }
+//   return (
+//     <>
+//       <h2>Hello {user} from Child component</h2>
+//       <button onClick={changeUserName}>Change to Cathy</button>
+//     </>
+//   );
+// }
+
 import {UserContext} from "./App13a"
 import { useContext } from "react";
 export default function Childa() {
   const {user,setUser} = useContext(UserContext);
-  const changeUserName=()=>{
-    setUser("Cathy")
-  }
   return (
     <>
       <h2>Hello {user} from Child component</h2>
-      <button onClick={changeUserName}>Change to Cathy</button>
+      <button>Change to Cathy</button>
     </>
   );
 }
